3ebf9925c58e99335d20a28e57acc9fd82a0f504
Benja <benjaminrojasdelpino@gmail.com>
2021-08-26 02:44:51 -0400

    Proyecto inicial subido

---
+++ src/Componentes/App.js
@@ -0,0 +1,18 @@
+import "../Assets/App.css"
+
+function App() {
+  return (
+    <div>
+      <span><center> ¿Que tipo de terreno quieres? </center></span>
+      <div className="Contenedor_1">
+        <div className="Contenedor_Izquierdo">
+          hola
+        </div>
+        <div className="Contenedor_Derecho">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
